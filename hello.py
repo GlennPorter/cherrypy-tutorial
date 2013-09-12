df83a566d09e71489a43efde6875a39320073e63
Glenn Porter <glenn.porter@live.com>
2013-09-12 10:38:28 +0100

    More.

--- hello.py
+++ hello.py
@@ -1,6 +1,13 @@
 import cherrypy
 
+class OnePage(object):
+	def index(self):
+		return "One page!"
+	index.exposed = True
+
 class HelloWorld:
+	onepage = OnePage()
+	
 	def index(self):
 		return "Hello world!"
 	index.exposed = True
